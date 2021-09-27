6068e4aa5baaa91c797a9d85926e7e01084fefb7
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2021-09-27 15:51:21 -0300

    initial project

---
+++ pokemon-album/pokemon_albumApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemon_albumApp.swift
+//  pokemon-album
+//
+//  Created by Sthefanny Gonzaga on 27/09/21.
+//
+
+import SwiftUI
+
+@main
+struct pokemon_albumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
